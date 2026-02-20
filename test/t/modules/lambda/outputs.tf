137eb368ce1ea624cf6b88539e45cc13e4154450
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-02-20 20:57:08 +0100

    s3 policy

--- test/t/modules/lambda/outputs.tf
+++ test/t/modules/lambda/outputs.tf
@@ -1,35 +1,29 @@
-output "invoke_arn" {
-  value = aws_lambda_function.this.invoke_arn
-}
-
-output "function_name" {
-  value = aws_lambda_function.this.function_name
-}
-
-resource "aws_iam_policy" "lambda_s3_access" {
-  name = "lambda-s3-access-policy"
+resource "aws_s3_bucket_policy" "allow_lambda_access" {
+  bucket = aws_s3_bucket.this.id
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
+        Sid    = "AllowLambdaObjectAccess"
         Effect = "Allow"
+        Principal = {
+          AWS = var.lambda_role_arns
+        }
         Action = [
           "s3:GetObject",
           "s3:PutObject"
         ]
-        Resource = [
-          "${aws_s3_bucket.existing_bucket.arn}/*"
-        ]
+        Resource = "${aws_s3_bucket.this.arn}/*"
       },
       {
+        Sid    = "AllowLambdaListAccess"
         Effect = "Allow"
-        Action = [
-          "s3:ListBucket"
-        ]
-        Resource = [
-          aws_s3_bucket.existing_bucket.arn
-        ]
+        Principal = {
+          AWS = var.lambda_role_arns
+        }
+        Action   = "s3:ListBucket"
+        Resource = aws_s3_bucket.this.arn
       }
     ]
   })
