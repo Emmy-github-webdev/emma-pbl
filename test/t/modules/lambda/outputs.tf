04724fabd6e37086daecc1c730037a8d6328fbb5
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-02-20 18:07:59 +0100

    iam policy

--- test/t/modules/lambda/outputs.tf
+++ test/t/modules/lambda/outputs.tf
@@ -5,3 +5,32 @@ output "invoke_arn" {
 output "function_name" {
   value = aws_lambda_function.this.function_name
 }
+
+resource "aws_iam_policy" "lambda_s3_access" {
+  name = "lambda-s3-access-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject"
+        ]
+        Resource = [
+          "${aws_s3_bucket.existing_bucket.arn}/*"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket"
+        ]
+        Resource = [
+          aws_s3_bucket.existing_bucket.arn
+        ]
+      }
+    ]
+  })
+}
